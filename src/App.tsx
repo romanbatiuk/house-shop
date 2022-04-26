fd22e52c73bb7d73cab8854122ff67ce277cdfe9
romanbatiuk <roman.batiuk87@gmail.com>
2022-04-26 22:08:04 +0300

    added pages and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Explore from './pages/Explore';
+import Offers from './pages/Offers';
+import ForgotPassword from './pages/ForgotPassword';
+// import Profile from './pages/Profile';
+import Signup from './pages/Signup';
+import Signin from './pages/Signin';
+
 function App() {
 	return (
-		<div>
-			<h1>House Shop!!</h1>
-		</div>
+		<>
+			<Router>
+				<Routes>
+					<Route path="/" element={<Explore />} />
+					<Route path="/offers" element={<Offers />} />
+					<Route path="/profile" element={<Signin />} />
+					<Route path="/sign-in" element={<Signin />} />
+					<Route path="/sign-up" element={<Signup />} />
+					<Route path="/forgot-password" element={<ForgotPassword />} />
+				</Routes>
+			</Router>
+		</>
 	);
 }
 
